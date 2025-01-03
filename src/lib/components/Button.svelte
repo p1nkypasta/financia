67115283ce5a69a9e154cb2b2f4805236edcf193
EleKtr1X <aarnav.thite@gmail.com>
2025-01-03 00:57:40 -0500

    add the buttons

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let label;
+  export let href;
+
+  import { goto } from "$app/navigation";
+</script>
+
+<div class="button-wrapper">
+  <div class="button" on:click={goto(href)} on:keypress={goto(href)} role="button" tabindex=0>
+    {label}
+  </div>
+</div>
+
+<style>
+  .button {
+    padding: 10px;
+    background-color: #0099ff;
+    color: white;
+    text-align: center;
+    border-radius: 10px;
+    width: 50px;
+    cursor: pointer;
+  }
+</style>
