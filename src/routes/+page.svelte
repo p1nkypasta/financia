a87220b888f2d42b95acca4dcff9f048f4173452
EleKtr1X <aarnav.thite@gmail.com>
2025-01-13 23:31:17 -0500

    fix pages not working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script>
     import Button from "../lib/components/Button.svelte";
 
+    import { base } from "$app/paths";
 </script>
 
 <div class="buttons">
-  <Button label="1" href="/1"></Button>
-  <Button label="2" href="/2"></Button>
-  <Button label="3" href="/3"></Button>
-  <Button label="4" href="/4"></Button>
+  <Button label="1" href="{base}/1"></Button>
+  <Button label="2" href="{base}/2"></Button>
+  <Button label="3" href="{base}/3"></Button>
+  <Button label="4" href="{base}/4"></Button>
 </div>
 
 <style>
